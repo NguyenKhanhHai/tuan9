1e8fc953ba33ceaedd69b9d17fac56daeeadbbcd
NguyenKhanhHai <haikpdz@gmail.com>
2025-11-04 07:26:52 +0700

    Delete IQuanLySach.java

--- IQuanLySach.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface IQuanLySach {
-    void themSach(Sach sach);
-    Sach timKiemSachTheoMa(String maSach);
-    // Có thể trả về nhiều kết quả nếu tìm theo tiêu đề - ở đây đơn giản theo mã
-    boolean xoaSach(String maSach);
-    List<Sach> hienThiDanhSach();
-}
